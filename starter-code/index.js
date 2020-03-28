eb3e35858675693437c0788e35c77a2a1191b8c1
mariehardy <mademoiselle.mph@gmail.com>
2020-03-28 17:12:15 +0100

    BER WDPT Marie - all done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length++
+    // .sort()
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos]
+    }
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length - 1] // OR this.items.pop()
+    }
+  }
 
-  max() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0] // OR this.items.shift()
+    }
+  }
 
-  min() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((sum, n) => sum + n, 0)
+    }
+  }
 
-  sum() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length
+    }
+  }
 
-  avg() {}
 }
 
 module.exports = SortedList;
